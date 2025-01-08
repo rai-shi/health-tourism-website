58a09b016c67ca3d8cc889aa31eb2dec44720a2f
Aysenur <aysenurtak1@gmail.com>
2025-01-08 14:20:07 +0300

    patient model is added

--- backend/web_backend/patient/models.py
+++ backend/web_backend/patient/models.py
@@ -1,2 +1,28 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser 
+from users.models import User
+from django.core.validators import RegexValidator
+
+# extend patient model with base User model
+class Patient(User):
+    gender_choices = {
+        "w": "woman",
+        "m": "man",
+    }
+    country_choices = {
+
+    }
+    city_choices = {
+
+    }
+    # r'^\+?1?\d{9,15}$'
+    phone_regex = RegexValidator(regex=r'^\d{10}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+    
+    gender = models.CharField(max_length=1, choices=gender_choices) 
+    birthday = models.DateField()
+    # no + and country code
+    phone_number = models.CharField(validators=[phone_regex], max_length=10, blank=True) 
+    country = models.CharField(max_length=3, choices=country_choices)
+    city = models.CharField(max_length=1, choices=city_choices) 
+    # (YYYY-MM-DD HH:MM[:ss[.uuuuuu]][TZ])
+    created_at = models.DateTimeField(auto_now_add=True)
--- backend/web_backend/users/models.py
+++ backend/web_backend/users/models.py
@@ -4,15 +4,16 @@ from django.contrib.auth.models import AbstractUser
 
 # extend user model with default abstract user
 class User(AbstractUser):
+    # all the field is not null and blank, everthing is required
     name = models.CharField(max_length=50)
     surname = models.CharField(max_length=50)
-    email = models.EmailField(max_length=50, unique=True)
+    email = models.EmailField(max_length=100, unique=True)
     password = models.CharField(max_length=100)
     username = None # required for the abstract user
     
     USERNAME_FIELD = "email" 
     # django authenticate with username and password but we will use email and password
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = [name, surname, email, password, username]
 
     # how user raw object will return 
     def __str__(self):
